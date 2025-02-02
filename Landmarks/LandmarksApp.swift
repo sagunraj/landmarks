e69efe0529fa11b6a17006540057392f8e055587
Sagun Raj Lage <sagoonraj@gmail.com>
2025-02-02 16:20:03 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sagun Raj Lage on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
