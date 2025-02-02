3f498718c13fa0e730e125f269419726a3b9ba2d
Sagun Raj Lage <sagoonraj@gmail.com>
2025-02-02 17:15:10 -0600

    adds the basic home layout with map view and circle image

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Sagun Raj Lage on 2/2/25.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
